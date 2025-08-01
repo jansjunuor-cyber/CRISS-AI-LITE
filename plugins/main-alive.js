69837af690197ff8f6760574b5c862d331940bc1
criss-vevo <crissvevo@icloud.com>
2025-08-01 21:16:04 +0300

    🧠 CRISS AI

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -39,8 +39,8 @@ lite({
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363402507750390@newsletter',
-                    newsletterName: 'Malvin Tech',
+                    newsletterJid: '120363417599637828@newsletter',
+                    newsletterName: 'CRISS AI',
                     serverMessageId: 143
                 }
             }
